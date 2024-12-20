4047820472dfbd6a8ab4bad5b50bb3eb275f0816
Sahil Amberkar <sahilamberkar2003@gmail.com>
2024-12-20 11:52:15 +0530

    Task 1 completed

---
+++ Javascript/Day3/script.js
@@ -0,0 +1,12 @@
+let taskarr = [
+  10,
+  20,
+  30,
+  ["quickxpert", "infotech"],
+  40,
+  [true, false],
+  ["good", 100, "day"],
+  ["apple", "mango", "grapes"],
+];
+
+document.write(taskarr[3][1] + "<br>" + taskarr[6][1] + "<br>" + taskarr[7][2]);
