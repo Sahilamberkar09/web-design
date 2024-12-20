6b4074e30cea2bd713596d372de82f734ccdd6ef
Sahil Amberkar <sahilamberkar2003@gmail.com>
2024-12-20 12:42:40 +0530

    Array Object Task Completed

--- Javascript/Day3/script.js
+++ Javascript/Day3/script.js
@@ -1,12 +1,52 @@
-let taskarr = [
+//Array
+// let taskarr = [
+//   10,
+//   20,
+//   30,
+//   ["quickxpert", "infotech"],
+//   40,
+//   [true, false],
+//   ["good", 100, "day"],
+//   ["apple", "mango", "grapes"],
+// ];
+
+// document.write(taskarr[3][1] + "<br>" + taskarr[6][1] + "<br>" + taskarr[7][2]);
+
+// ===============================================================================================================
+
+// Ojbects
+// let emp = {
+//   name: "ankit",
+//   city: "thane",
+//   id: 101,
+// };
+
+// document.write(emp.name); //Method 1
+// document.write(emp["name"]); //Method 2
+
+// ===============================================================================================================
+
+//Accessing Objects inside an Array
+
+const taskArrobj = [
   10,
   20,
   30,
-  ["quickxpert", "infotech"],
-  40,
+  "sam",
   [true, false],
-  ["good", 100, "day"],
-  ["apple", "mango", "grapes"],
+  {
+    hobby: ["sing", "dance", "swim"],
+  },
+  { city: "Kerala", id: 101 },
 ];
 
-document.write(taskarr[3][1] + "<br>" + taskarr[6][1] + "<br>" + taskarr[7][2]);
+document.write(
+  "This is " +
+    taskArrobj[4][0] +
+    "<br>This is my hobby : " +
+    taskArrobj[5].hobby[1] +
+    "<br>This is my id : " +
+    JSON.stringify(taskArrobj[6].id)
+);
+
+// ===============================================================================================================
