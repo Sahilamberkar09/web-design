941e0c752d6f76f033990eedc22b3bc3bad9513f
Sahil Amberkar <sahilamberkar2003@gmail.com>
2024-12-24 12:30:12 +0530

    Day 5 Compeleted

---
+++ Javascript/Day5/dom.js
@@ -0,0 +1,12 @@
+function addimg() {
+  var img = document.getElementById("dynimg");
+  img.setAttribute(
+    "src",
+    "https://cdn.pixabay.com/photo/2022/11/13/21/32/holly-7590229_640.jpg"
+  );
+}
+
+function addborder() {
+  var img = document.getElementById("dynimg");
+  img.style.border = "10px solid red";
+}
